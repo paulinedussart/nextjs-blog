faa35efdbd01eb8588c132023159b6b0185c79fd
paulinedussart <pauline.dussart@jobteaser.com>
2023-01-13 16:25:07 +0100

    feat(Post): build first navigation using Link

---
+++ pages/posts/first-post.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+const FirstPost = () => {
+  return (
+    <>
+      <h1>First post</h1>
+      <Link href="/">Home Page</Link>
+    </>
+  )
+}
+export default FirstPost
